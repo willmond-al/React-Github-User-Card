0a9e85b04cd688ee559e8f5e44931ecff922232b
willmondal <wbmondal@aol.com>
2021-01-13 09:58:31 -0400

    axios call made within component did mount and user info has been rendered on the dom

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,8 +1,37 @@
 import logo from './logo.svg';
+import React from 'react'
+import axios from 'axios'
 import './App.css';
 
-function App() {
- 
+class App extends React.Component{
+
+  state ={
+    gitUserInfo: [],
+    gitUser: "willmond-al"
+  }
+
+  componentDidMount(){
+    axios
+    .get(`https://api.github.com/users/willmond-al`)
+    .then(res =>{
+      this.setState({
+        gitUserInfo: res.data
+      })
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  render(){
+    return(
+      <div>
+      <h1>GitHub User Card</h1>
+    <p>{this.state.gitUserInfo.name} has {this.state.gitUserInfo.followers} followers on gitHub!</p>
+    </div>
+    )
+  }
+
 }
 
 export default App;
